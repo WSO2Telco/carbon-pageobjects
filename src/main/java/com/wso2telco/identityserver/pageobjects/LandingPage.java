cefcca033654326f087ec37fa0f8f8ec4a3ae86d
Susantha Pathirana <Susantha Pathirana>
2016-10-28 16:21:51 +0530

    Updated with src folder

---
+++ src/main/java/com/wso2telco/identityserver/pageobjects/LandingPage.java
@@ -0,0 +1,128 @@
+package com.wso2telco.identityserver.pageobjects;
+
+import org.openqa.selenium.WebDriver;
+
+import com.wso2telco.test.framework.core.WebPelement;
+import com.wso2telco.test.framework.util.UIType;
+
+public class LandingPage extends BasicPageObject  {
+	
+	private WebPelement lblUsername = defineEelement(UIType.Xpath, ".//div[@id='middle']/h2");
+	private WebPelement menuConfigure = defineEelement(UIType.ID, "menu-panel-button3");
+	private WebPelement lblConfigure = defineEelement(UIType.ID, "region1_configure_menu");
+	private WebPelement lnkUsersRoles = defineEelement(UIType.Xpath, ".//a[contains(text(),'Users and Roles')]");
+	private WebPelement lnkUsers = defineEelement(UIType.Xpath, ".//table[@id='internal']/tbody/tr[1]/td/a");
+	private WebPelement txtUserName = defineEelement(UIType.Name, "org.wso2.carbon.user.filter");
+	private WebPelement btnSearch = defineEelement(UIType.Xpath, ".//input[@value='Search']");
+	private WebPelement btnDelete = defineEelement(UIType.Xpath, ".//a[contains(text(),'Delete')]");
+	private WebPelement btnConfirmarionYes = defineEelement(UIType.Css, ".ui-dialog-buttonpane>button");
+	private WebPelement lblNoUserMessage = defineEelement(UIType.ID, "messagebox-info");
+	private WebPelement btnUserProfile = defineEelement(UIType.Xpath, ".//a[contains(text(),'Profile')]");
+	
+	
+	
+	
+	
+	
+	public LandingPage(WebDriver driver) {
+		super(driver);
+		// TODO Auto-generated constructor stub
+	}
+
+	public boolean isISHomePage(String pageTitle) throws Exception {
+		flag = false;
+		logInstruction("Validating IS Home Page");
+		try {
+			if (pageTitle.contains(getElement(lblUsername).getText())) {
+				flag = true;
+				logInstruction("Validating IS Home Page completed");
+			} else {
+				logInstruction("Title is Not Matched");
+			}
+		} catch (Exception e) {
+			logInstruction("Exception While Validating IS Home Page 'isISHomePage()'"
+					+ e.getMessage());
+			throw new Exception(
+					"Exception While Validating IS Home Page 'isISHomePage()'"
+							+ e.getLocalizedMessage());
+		}
+		return flag;
+	}
+	
+	public void clickConfigure(){
+		getElement(menuConfigure).click();		
+	}
+	
+	public boolean isConfigurePage(String pageTitle) throws Exception {
+		flag = false;
+		logInstruction("Validating Configure Label");
+		try {
+			if (pageTitle.contains(getElement(lblConfigure).getText())) {
+				flag = true;
+				logInstruction("Validating Configure Label completed");
+			} else {
+				logInstruction("Configure Label not displayed");
+			}
+		} catch (Exception e) {
+			logInstruction("Exception While Validating Configure Label 'isConfigurePage()'"
+					+ e.getMessage());
+			throw new Exception(
+					"Exception While Validating Configure Label 'isConfigurePage()'"
+							+ e.getLocalizedMessage());
+		}
+		return flag;
+	}
+	
+	public void clickUserRoles(){
+		getElement(lnkUsersRoles).click();		
+	}
+	
+	public void clickUsers(){
+		getElement(lnkUsers).click();		
+	}
+	
+	public void enterUserName(String msisdn){
+		getElement(txtUserName).clear();
+		getElement(txtUserName).sendKeys(msisdn);	
+	}
+	
+	public void clickSearch(){
+		getElement(btnSearch).click();	
+	}
+
+	public void clickDelete(){
+		getElement(btnDelete).click();	
+		getElement(btnConfirmarionYes).click();	
+
+	}
+	
+
+	public boolean isUserDeleted(String message) throws Exception {
+		flag = false;
+		logInstruction("Validating user is deleted");
+		try {
+			if (message.contains(getElement(lblNoUserMessage).getText())) {
+				flag = true;
+				logInstruction("Validating user is deleted completed");
+			} else {
+				logInstruction("user is not deleted");
+			}
+		} catch (Exception e) {
+			logInstruction("Exception While Validating user deletion message 'isUserDeleted()'"
+					+ e.getMessage());
+			throw new Exception(
+					"Exception While Validating user deletion message 'isUserDeleted()'"
+							+ e.getLocalizedMessage());
+		}
+		return flag;
+	}
+
+	public UserProfile clickUserProfile() {
+		// TODO Auto-generated method stub
+		getElement(btnUserProfile).click();
+		return new UserProfile(driver);
+	}
+
+
+	
+}
---
+++ src/main/java/com/wso2telco/identityserver/pageobjects/carbon/CarbonResourceBrowse.java
@@ -0,0 +1,225 @@
+package com.wso2telco.identityserver.pageobjects.carbon;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import com.wso2telco.identityserver.pageobjects.BasicPageObject;
+import com.wso2telco.test.framework.core.WebPelement;
+import com.wso2telco.test.framework.util.UIType;
+
+/**
+ * The Class CarbonResourceBrowse.
+ */
+public class CarbonResourceBrowse extends BasicPageObject {
+
+	/**
+	 * Instantiates a new carbon resource browse.
+	 *
+	 * @author SulakkhanaW
+	 * @param driver the driver
+	 */
+	public CarbonResourceBrowse(WebDriver driver) {
+		super(driver);
+	}
+	
+	/** The logger. */
+	Logger logger = Logger.getLogger(CarbonHomePage.class);
+	
+	/** The lbl page header. */
+	private WebPelement lblPageHeader = defineEelement(UIType.Xpath, "//div[@id='middle']/h2");
+	
+	/** The txt location. */
+	private WebPelement txtLocation = defineEelement(UIType.Xpath, "//input[@id='uLocationBar']");
+	
+	/** The btn go. */
+	private WebPelement btnGo = defineEelement(UIType.Xpath, "//input[@value='Go']");
+	
+	/** The lnk display as text. */
+	private String lnkDisplayAsText = "//td/a[contains(text(),'Display as text')]";
+	
+	/** The lnk display as text element. */
+	private WebPelement lnkDisplayAsTextElement = defineEelement(UIType.Xpath, lnkDisplayAsText);
+	
+	/** The txt rate card. */
+	private WebPelement txtRateCard = defineEelement(UIType.Xpath, "//div[@id='generalContentDiv']/textarea");
+	
+	/** The lnk upload. */
+	private WebPelement lnkUpload = defineEelement(UIType.Xpath, "//td/a[contains(.,'Upload')]");
+	
+	/** The txt upload location. */
+	private WebPelement txtUploadLocation = defineEelement(UIType.ID, "uResourceFile");
+	
+	/** The btn uplaod. */
+	private WebPelement btnUplaod = defineEelement(UIType.ID, "uploadContentButtonID");
+	
+	/**
+	 * Checks if is browse page.
+	 *
+	 * @author SulakkhanaW
+	 * @param pageHeader the page header
+	 * @return true, if is browse page
+	 * @throws Exception the exception
+	 */
+	public boolean isBrowsePage(String pageHeader) throws Exception{
+		flag = false;
+		logger.debug("Validating resources browse page");
+		Thread.sleep(sleepTime);
+		try {
+			if (pageHeader.contains(getElement(lblPageHeader).getText().trim())) {
+				flag = true;
+				logger.debug("Validating resources browse page completed");
+			} else {
+				logger.debug("Resources browse page title Not Matched");
+			}
+		} catch (Exception e) {
+			logger.debug("Exception While Validating resources browse page, Page Title 'isBrowsePage()'" + e.getMessage());
+			throw new Exception("Exception While Validating resources browse page, Page Title 'isBrowsePage()'" + e.getLocalizedMessage());
+		}
+		return flag;
+	}
+	
+	/**
+	 * Serach rate card.
+	 *
+	 * @author SulakkhanaW
+	 * @param location the location
+	 */
+	public void serachRateCard(String location){
+		logger.debug("Entering location");
+		getElement(txtLocation).clearAndSendkeys(location);
+		logger.debug("Location entered");
+		logger.debug("Clicking on Go");
+		getElement(btnGo).click();
+		logger.debug("Clicked on Go");
+	}
+	
+	/**
+	 * Checks if is rate card search.
+	 *
+	 * @author SulakkhanaW
+	 * @return true, if is rate card search
+	 * @throws Exception the exception
+	 */
+	public boolean isRateCardSearch() throws Exception{
+		flag = false;
+		logger.debug("Validating rate card search");
+		List<WebElement> element = driver.findElements(By.xpath(lnkDisplayAsText));
+		try {
+			if (element.size() == 1){
+				flag = true;
+				logger.debug("Rate card searched");
+			} else {
+				logger.debug("Rate card search failed");
+			}
+		} catch (Exception e) {
+			logger.debug("Exception While Validating rate card search 'isRateCardSearch()'" + e.getMessage());
+			throw new Exception("Exception While Validating rate card search 'isRateCardSearch()'" + e.getLocalizedMessage());
+		}
+		return flag;
+	}
+	
+	/**
+	 * Click display as text.
+	 *
+	 * @author SulakkhanaW
+	 */
+	public void clickDisplayAsText(){
+		logger.debug("Clicking on Displays as Text link");
+		getElement(lnkDisplayAsTextElement).click();
+		logger.debug("Clicked on Displays as Text link");
+	}
+	
+	/**
+	 * Gets the rate card.
+	 *
+	 * @author SulakkhanaW
+	 * @return the rate card
+	 */
+	public String getRateCard(){
+		logger.debug("Finding rate card text area");
+		String ratecard = getElement(txtRateCard).getText();
+		logger.debug("Rate card text area found");
+		return ratecard;
+	}
+	
+	/**
+	 * Upload rate card.
+	 *
+	 * @author SulakkhanaW
+	 * @param filename the filename
+	 * @throws InterruptedException 
+	 */
+	public void uploadRateCard(String filename) throws InterruptedException{
+		logger.debug("Clicking on Upload link");
+		getElement(lnkUpload).click();
+		logger.debug("Clicked on Upload link");
+		Thread.sleep(sleepTime);
+		String browserType = config.getValue("browser");
+		if (browserType.equals("FIREFOX")){
+			logger.debug("Sending upload file name");
+			selectFileFFox(filename);
+			logger.debug("Upload file name send");	
+		} else {
+			logger.debug("Sending upload file name");
+			getElement(txtUploadLocation).clearAndSendkeys(filename);
+			logger.debug("Upload file name send");			
+		}		
+		logger.debug("Clicking on Upload button");
+		Thread.sleep(sleepTime);
+		getElement(btnUplaod).click();
+		logger.debug("Clicked on Upload button");
+	}
+	
+	/**
+	 * Select file ff.
+	 *
+	 * @param filePath the file path
+	 * @throws InterruptedException 
+	 */
+	public void selectFileFFox(String filePath) throws InterruptedException{
+		String file  = filePath.replaceAll("/", "\\\\");
+		getElement(txtUploadLocation).click();
+		Thread.sleep(sleepTime);
+		selectFileFromPopup(file);
+	}
+	
+	/**
+	 * Select file from popup.
+	 *
+	 * @param filePath the file path
+	 */
+	public static void selectFileFromPopup(String filePath){
+		setClipboardData(filePath);
+		try {
+			Robot robot = new Robot();
+			robot.keyPress(KeyEvent.VK_CONTROL);
+			robot.keyPress(KeyEvent.VK_V);
+			robot.keyRelease(KeyEvent.VK_V);
+			robot.keyRelease(KeyEvent.VK_CONTROL);
+			robot.keyPress(KeyEvent.VK_ENTER);
+			robot.keyRelease(KeyEvent.VK_ENTER);
+		} catch (AWTException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Sets the clipboard data.
+	 *
+	 * @param filePath the new clipboard data
+	 */
+	public static void setClipboardData(String filePath){
+		StringSelection stringSelection = new StringSelection(filePath);
+		Toolkit.getDefaultToolkit().getSystemClipboard().setContents(stringSelection, null);
+	}
+
+}
