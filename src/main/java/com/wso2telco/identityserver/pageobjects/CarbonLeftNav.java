327c9227908a4d95f8eb82468c1da37565efcf3e
tharinduwso2telco <tharindu@wso2telco.com>
2017-07-12 16:51:14 +0530

    Add methods to map hub claims and mig claims

--- src/main/java/com/wso2telco/identityserver/pageobjects/CarbonLeftNav.java
+++ src/main/java/com/wso2telco/identityserver/pageobjects/CarbonLeftNav.java
@@ -27,6 +27,24 @@ public class CarbonLeftNav extends BasicPageObject {
 			UIType.Xpath,
 			"//ul[@class='main']//li[contains(text(),'Claims')]/following-sibling::li[1]/ul/li/a[text()='Add']");
 
+	private WebPelement wso2orgclaims = defineEelement(UIType.Xpath,".//*[@id='workArea']/table/tbody/tr[6]/td[1]/a");
+	
+	private WebPelement emailEdit = defineEelement(UIType.Xpath, ".//*[@id='workArea']/div[19]/a[2]");
+	private WebPelement firstNameEdit = defineEelement(UIType.Xpath, ".//*[@id='workArea']/div[23]/a[2]");
+	private WebPelement lastNameEdit = defineEelement(UIType.Xpath, ".//*[@id='workArea']/div[33]/a[2]");
+	
+	private WebPelement claimRequire = defineEelement(UIType.Xpath, ".//*[@id='required']");
+	
+	private WebPelement claimUpdate = defineEelement(UIType.Xpath, ".//*[@id='workArea']/form/table/tbody/tr[2]/td/input[1]");
+	
+	private WebPelement shutdownRestart = defineEelement(UIType.Xpath, ".//*[@id='menu']/ul/li[9]/ul/li[7]/a");
+	
+	private WebPelement gracefulRestart = defineEelement(UIType.Xpath, ".//*[@id='restart']/tbody/tr[3]/td[1]/a");
+	
+	private WebPelement clickYes = defineEelement(UIType.Xpath, "html/body/div[3]/div[2]/button[1]");
+	
+	private WebPelement clickOK = defineEelement(UIType.Xpath, "html/body/div[3]/div[2]/button");
+	
 	public CarbonLeftNav(WebDriver driver) {
 		super(driver);
 		// TODO Auto-generated constructor stub
@@ -68,5 +86,45 @@ public class CarbonLeftNav extends BasicPageObject {
 
 		return new ClaimsAddPage(driver);
 
+	}
+	
+	public void clickWSO2OrgClaims(){
+		getElement(wso2orgclaims).click();
+	}
+	
+	public void clickEmailEdit(){
+		getElement(emailEdit).click();
+	}
+	
+	public void clickFirstNameEdit(){
+		getElement(firstNameEdit).click();
+	}
+	
+	public void clickLastNameEdit(){
+		getElement(lastNameEdit).click();
+	}
+	
+	public void clickClaimrequire(){
+		getElement(claimRequire).click();
+	}
+	
+	public void clickClaimUpdate(){
+		getElement(claimUpdate).click();
+	}
+	
+	public void clickShutdownRestart(){
+		getElement(shutdownRestart).click();
+	}
+	
+	public void clickGracefulRestart(){
+		getElement(gracefulRestart).click();
+	}
+	
+	public void clickYes(){
+		getElement(clickYes).click();
+	}
+	
+	public void clickOK(){
+		getElement(clickOK).click();
 	}
 }
